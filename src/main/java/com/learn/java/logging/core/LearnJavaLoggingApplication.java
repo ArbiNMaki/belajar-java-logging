f4a35f398476587d6cf168e88c7d2ec6178b9d6d
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-23 11:53:29 +0700

    first

---
+++ src/main/java/com/learn/java/logging/core/LearnJavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.java.logging.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/java/logging/core/LearnJavaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java.logging.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
